ed7b88da39534e45eb3ec364f3815c69b5ca0984
Krish-Ideausher <77826469+Krish-Ideausher@users.noreply.github.com>
2021-06-26 17:13:46 +0530

    added option for scaled ui on screensize

--- lib/extension.dart
+++ lib/extension.dart
@@ -1,6 +1,9 @@
 part of sizer;
 
 extension SizerExt on num {
+  static double scaleFactorH = 0.11;
+  static double scalefactorW = 0.26;
+
   /// Calculates the height depending on the device's screen size
   ///
   /// Eg: 20.h -> will take 20% of the screen's height
@@ -13,4 +16,11 @@ extension SizerExt on num {
 
   /// Calculates the sp (Scalable Pixel) depending on the device's screen size
   double get sp => this * (SizerUtil.width / 3) / 100;
+
+  double get kh => (this * SizerUtil.height * scaleFactorH) / 100;
+
+  /// Calculates the width depending on the device's screen size
+  ///
+  /// Eg: 20.kw -> will take 20% of the screen's width
+  double get kw => (this * SizerUtil.width * scalefactorW) / 100;
 }
