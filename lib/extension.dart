7834842c12cf048c8fabee9de8226cf6f2bee41f
Krish-Ideausher <77826469+Krish-Ideausher@users.noreply.github.com>
2021-06-26 17:18:17 +0530

    comment updated

--- lib/extension.dart
+++ lib/extension.dart
@@ -1,8 +1,8 @@
 part of sizer;
 
 extension SizerExt on num {
-  static double scaleFactorH = 0.11;
-  static double scalefactorW = 0.26;
+  static double scaleFactorH = 0.23;
+  static double scalefactorW = 0.24;
 
   /// Calculates the height depending on the device's screen size
   ///
@@ -17,10 +17,13 @@ extension SizerExt on num {
   /// Calculates the sp (Scalable Pixel) depending on the device's screen size
   double get sp => this * (SizerUtil.width / 3) / 100;
 
+  /// Calculates the number of pixel depending on the device's screen size
+  ///
+  /// Eg: 20.kh -> will take 20 logical pixel of the screen's height
   double get kh => (this * SizerUtil.height * scaleFactorH) / 100;
 
-  /// Calculates the width depending on the device's screen size
+  /// Calculates the number of pixel depending on the device's screen size
   ///
-  /// Eg: 20.kw -> will take 20% of the screen's width
+  /// Eg: 20.kw -> will take 20 logical pixel of the screen's width
   double get kw => (this * SizerUtil.width * scalefactorW) / 100;
 }
